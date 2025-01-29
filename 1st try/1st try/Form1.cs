ad2b901e7a7f6430817c535f6684cbb0c7aab200
KaloyanYodanov <kyordanov922@gmail.com>
2025-01-29 12:38:54 +0200

    Rotors rotation done

--- 1st try/1st try/Form1.cs
+++ 1st try/1st try/Form1.cs
@@ -1,7 +1,8 @@
-//TO DO
-/*
- -New message button which clears label5 and enables radioButtons and comboBoxes 
- */
+/*TO DO
+
+- New message button which clears label5 and richtextBox and enables radioButtons and comboBoxes 
+- Dictionary to store the relations between the rotors
+*/
 using System.Security.Cryptography;
 using System.Windows.Forms;
 namespace _1st_try
@@ -11,36 +12,16 @@ namespace _1st_try
         public Form1()
         {
             InitializeComponent();
-            InitialiseAlphabet();
         }
 
-        private List<char> alphabet = new List<char>();
         private List<char> rotor_1 = new List<char> { 'N', 'O', 'C', 'W', 'L', 'H', 'P', 'R', 'K', 'Y', 'U', 'Q', 'V', 'I', 'J', 'D', 'E', 'M', 'G', 'F', 'T', 'A', 'B', 'S', 'Z', 'X' };
         private List<char> rotor_2 = new List<char> { 'C', 'E', 'J', 'A', 'Q', 'V', 'D', 'G', 'U', 'B', 'O', 'T', 'X', 'P', 'S', 'I', 'Y', 'F', 'N', 'M', 'W', 'Z', 'R', 'H', 'K', 'L' };
         private List<char> rotor_3 = new List<char> { 'S', 'V', 'E', 'Z', 'G', 'Y', 'I', 'K', 'J', 'N', 'W', 'T', 'B', 'O', 'M', 'Q', 'P', 'H', 'A', 'U', 'C', 'X', 'D', 'L', 'F', 'R' };
         bool flag_comboBox1 = false;
         bool flag_comboBox2 = false;
         bool flag_comboBox3 = false;
-        bool flag_initialisedRotors = false;
 
         #region Methods
-        private List<char> InitialiseAlphabet()
-        {
-            for (int i = 0; i < 26; i++)
-            {
-                alphabet.Add((char)(i + 65));
-            }
-            return alphabet;
-        }
-        //private List<char> ResetRotor(List<char> rotor)
-        //{
-        //    rotor.Clear();
-        //    for (int i = 0; i < 26; i++)
-        //    {
-        //        rotor.Add((char)(i + 65));
-        //    }
-        //    return rotor;
-        //}
         private List<char> RotateRotor(List<char> rotor, int rotations)
         {
             for (int i = 0; i < rotations; i++)
@@ -70,37 +51,52 @@ namespace _1st_try
             }
             return shuffled_arr.ToList();
         }
+        private List<char> ResetRotor(List<char> rotor)
+        {
+            rotor.Clear();
+            for (int i = 0; i < 26; i++)
+            {
+                rotor.Add((char)(i + 65));
+            }
+            return rotor;
+        }
         #endregion
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            RotateRotor(rotor_1, int.Parse(comboBox1.Text) - 1);
-            flag_comboBox1 = true;
+            if (!flag_comboBox1)
+            {
+                RotateRotor(rotor_1, int.Parse(comboBox1.Text));
+                flag_comboBox1 = true;
+            }
+            else
+            {
+                RotateRotor(rotor_1, 1);
+            }
         }
         private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            RotateRotor(rotor_2, int.Parse(comboBox2.Text) - 1);
-            flag_comboBox2 = true;
-        }
-        private void comboBox3_SelectedIndexChanged_2(object sender, EventArgs e)
-        {
-            if(!flag_comboBox3)
+            if (!flag_comboBox2)
             {
-                RotateRotor(rotor_3, int.Parse(comboBox3.Text) - 1);
-                flag_comboBox3 = true;
+                RotateRotor(rotor_2, int.Parse(comboBox2.Text));
+                flag_comboBox2 = true;
             }
             else
             {
                 RotateRotor(rotor_3, 1);
             }
         }
-        private void Form1_Load(object sender, EventArgs e)
+        private void comboBox3_SelectedIndexChanged_2(object sender, EventArgs e)
         {
-
-        }
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            if(!flag_comboBox3)
+            {
+                RotateRotor(rotor_3, int.Parse(comboBox3.Text));
+                flag_comboBox3 = true;
+            }
+            else
+            {
+                RotateRotor(rotor_3, 1);
+            }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -122,7 +118,6 @@ namespace _1st_try
                 //Rotors rotation
                 if (int.Parse(comboBox1.Text) == 26 && int.Parse(comboBox2.Text) % 4 == 0 && int.Parse(comboBox3.Text) % 6 == 0)//Previous -> int.Parse(comboBox1.Text) == 26
                 {
-                    RotateRotor(rotor_1, 1);
                     comboBox1.Text = 1.ToString();
                 }
                 else if (int.Parse(comboBox2.Text) % 4 == 0 && int.Parse(comboBox3.Text) % 6 == 0)
@@ -132,7 +127,6 @@ namespace _1st_try
 
                 if (int.Parse(comboBox2.Text) == 26 && int.Parse(comboBox3.Text) % 6 == 0)//Previous -> int.Parse(comboBox2.Text) == 26
                 {
-                    RotateRotor(rotor_2, 1);
                     comboBox2.Text = 1.ToString();
                 }
                 else if (int.Parse(comboBox3.Text) % 6 == 0)
@@ -142,10 +136,9 @@ namespace _1st_try
 
                 if (int.Parse(comboBox3.Text) == 26)
                 {
-                    RotateRotor(rotor_3, 1);
-                    comboBox3.Text = 0.ToString();//Previous -> comboBox3.Text = 1.ToString();
+                    comboBox3.Text = 1.ToString();//Previous -> comboBox3.Text = 1.ToString();
                 }
-                comboBox3.Text = (int.Parse(comboBox3.Text) + 1).ToString();
+                else comboBox3.Text = (int.Parse(comboBox3.Text) + 1).ToString();
 
                 //Ecnryption logic
                 char letter = richTextBox1.Text[richTextBox1.TextLength - 1];
@@ -195,13 +188,19 @@ namespace _1st_try
         {
 
         }
-
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
-
         private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
--- 1st try/1st try/obj/Debug/net8.0-windows/1st try.AssemblyInfo.cs
+++ 1st try/1st try/obj/Debug/net8.0-windows/1st try.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1st try")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d3d266fcb1698e17ce287b40ddeed612093a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52fe230591dd6068da16c9c26a116d95942e6bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("1st try")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1st try")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
